2dc67d1953fea20060b97b842e99acea2071dcb3
Hayes Chiasson <hchias2@lsu.edu>
2025-04-07 14:12:12 -0500

    Shop Page setup temp

--- app/shop/page.jsx
+++ app/shop/page.jsx
@@ -5,16 +5,28 @@
 import Link from "next/link";
 import Image from "next/image";
 
+import Poster from "@/public/assets/poster.png";
+
+import GameNavbar from "@/components/ui/game-navbar.jsx";
+
 import PacksDisplay from "@/components/ui/packs-display.jsx";
 
 
 export default function Shop() {
   return (
-    <div className="h-screen flex justify-center items-center fade-in">
-      <PacksDisplay/>
-      <div className="flex flex-col">
-        <p>Shop Page</p>
+    <>
+      <div className="top-6 right-[520px] absolute">
+      <Link href="/collection">
+          <GameNavbar/>
+      </Link>
       </div>
-    </div>
+      
+      <div className="h-screen flex justify-center items-center fade-in">
+        <PacksDisplay/>
+        <div className="flex flex-col">
+          <p>Shop Page</p>
+        </div>
+      </div>
+    </>
   );
 }
--- components/ui/packs-display.jsx
+++ components/ui/packs-display.jsx
@@ -1,27 +1,57 @@
+//replace w/ grid / margin beteen later
+
+// import Poster from "/assets/poster.png";
 
 function PacksDisplay () {
     return (
      <>
-     {/* 1st pack */}
-     
-        <div className="fixed h-[450px] w-[300px] bottom-80 bg-green-500/30 border border-[#4eff91]/30 rounded-lg p-4">
-            Iron Pack
+        <div className="fixed top-20 left-36 w-[480px] h-96 z-10">
+            <img src={'/assets/poster-2.svg'} width={1000} height={1000} alt="Poster">
+            </img>
+
         </div>
 
-        {/* 2nd pack */}
+        {/* 1st pack */}
+        
+            <div className="fixed h-[450px] w-[300px] bottom-80 right-[505px]  bg-green-500/30 border border-[#4eff91]/30 rounded-lg p-4">
+                Gold Pack
+            </div>
 
-        <div className="fixed h-[450px] w-[300px] bottom-80 left-64 bg-green-500/30 border border-[#4eff91]/30 rounded-lg p-4">
-            Gold Pack
-        </div>
+            {/* 2nd pack */}
 
-        {/* 3rd pack */}
+            <div className="fixed h-[450px] w-[300px] bottom-80  bg-green-500/30 border border-[#4eff91]/30 rounded-lg p-4">
+                Iron Pack
+            </div>
 
-        <div className="fixed h-[450px] w-[300px] bottom-80 right-64 bg-green-500/30 border border-[#4eff91]/30 rounded-lg p-4">
-            Diamond Pack
-        </div>
+            {/* 3rd pack */}
+
+            <div className="fixed h-[450px] w-[300px] bottom-80 right-24 bg-green-500/30 border border-[#4eff91]/30 rounded-lg p-4">
+                Diamond Pack
+            </div>
+
      </>
 
     )
 }
 
 export default PacksDisplay
+
+// <>
+// {/* 1st pack */}
+
+//    <div className="fixed h-[450px] w-[300px] bottom-80 bg-green-500/30 border border-[#4eff91]/30 rounded-lg p-4">
+//        Iron Pack
+//    </div>
+
+//    {/* 2nd pack */}
+
+//    <div className="fixed h-[450px] w-[300px] bottom-80 left-64 bg-green-500/30 border border-[#4eff91]/30 rounded-lg p-4">
+//        Gold Pack
+//    </div>
+
+//    {/* 3rd pack */}
+
+//    <div className="fixed h-[450px] w-[300px] bottom-80 right-64 bg-green-500/30 border border-[#4eff91]/30 rounded-lg p-4">
+//        Diamond Pack
+//    </div>
+// </>
