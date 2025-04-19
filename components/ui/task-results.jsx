a3d2205a74ba9fdb30708ba3b161081c94712355
Hayes Chiasson <hayeschiasson1@yahoo.com>
2025-04-19 13:35:25 -0500

    work on results page display

--- components/ui/task-results.jsx
+++ components/ui/task-results.jsx
@@ -3,10 +3,21 @@ function TaskResults() {
     return (
         <>
             <div className="absolute w-full h-full bg-[#0B0C2A]">
-                <div className="relative flex flex-col items-center top-[9%] h-full text-4xl">
+                <div className="relative flex flex-col items-center top-[9%] h-[1%] text-4xl">
                     Results
                 </div>
                 {/* Scores(progressbars) */}
+                <div className="relative flex flex-col items-center top-[30%] h-[30%] w-full text-2xl bg-purple-800">
+                    Progress
+                </div>
+                {/* Overall Score: */}
+                <div className="relative flex flex-col items-center top-[30%] h-[20%] w-full text-4xl bg-purple-300">
+                    Overall Score:
+                    <div className="pt-8 text-2xl">
+                        96% Survivability
+                    </div>
+                </div>
+
             </div>
         </>
     );
