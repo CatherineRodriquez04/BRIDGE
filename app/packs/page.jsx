0511730322354f60a0bcc47154b9901e6cd751d0
Hayes Chiasson <hchias2@lsu.edu>
2025-04-21 13:30:30 -0500

    update packs page

--- app/packs/page.jsx
+++ app/packs/page.jsx
@@ -25,7 +25,19 @@ export default function Packs() {
       <div className="flex items-center h-screen fade-in">
           {/* Poster New Char Display (make component?)*/}
           <div className="fixed w-[28%] h-full -z-1 left-[6%] top-[15%]">
-            <img src={'/assets/limited-paper-poster.svg'} width={8000} height={800} alt="Poster" className="-z-1"></img>
+            <img src={'/assets/limited-paper-poster.svg'} width={500} height={600} alt="Poster" className="-z-1"></img>
+            <img src={'/assets/limited-image-lean.svg'} width={380} height={430} alt="Poster" className="z-20 absolute top-[195px] left-[65px]"></img>
+
+            {/* <button type="button" className="flex items-center text-white hover:text-[#0B0C2A] border-[#C4F7BC] hover:[#0B0C2A] hover:bg-[#C4F7BC] active:ring-4 active:ring-[#C4F7BC] active:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-10 mx-auto border-2"></button> */}
+            <Link href="/shop">
+
+            <button
+                type="button"
+                className="absolute w-[40%] left-[30%] flex items-center justify-center bg-[#0B0C2A] text-white hover:text-[#0B0C2A] border-[#C4F7BC] hover:[#0B0C2A] hover:bg-[#C4F7BC] active:ring-4 active:ring-[#C4F7BC] active:outline-none font-medium rounded-lg text-4xl px-5 py-1.5 mt-2 border-4">
+              Go to Shop
+              </button>
+
+            </Link>
               {/* <div className="absolute h-[370px] w-[320px] skew-x-6 inset-0 transform origin-center  top-[48%] left-[50%] translate-x-[-50%] translate-y-[-50%] border-4 border-[#5E4112] text-center text-white text-4xl font-bold ">
 
               </div>  Inner box for char image in limited*/}
--- components/ui/packs-display.jsx
+++ components/ui/packs-display.jsx
@@ -9,30 +9,35 @@ function PacksDisplay () {
         {/* fixed h-[400px] w-[260px] bottom-80 right-[505px] */}
         <div className="flex space-x-8 top-[250px] absolute right-[230px]">
             <div> 
-                <div className="flex h-[400px] w-[260px] bg-[#0B0C2A] border-4 border-[#C4F7BC] rounded-lg p-4">
+                {/* <div className="flex h-[400px] w-[260px] bg-[#0B0C2A] border-4 border-[#C4F7BC] rounded-lg p-4">
                     <img src="/assets/temp-card-pack-1.svg" height={400} width={260} alt="Temp-Card-Pack-1" className="w-full h-full"></img>
-                </div>
-                {/* <but  ton className="justify-center place-items-center">Payment</button> */}
-                <button type="button" className="flex items-center text-white hover:text-[#0B0C2A] border-[#C4F7BC] hover:[#0B0C2A] hover:bg-[#C4F7BC] active:ring-4 active:ring-[#C4F7BC] active:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-8 mx-auto border-2">300 coins</button>
+                </div> */}
+                <img src="/assets/temp-card-pack-1.svg" height={400} width={260} alt="Temp-Card-Pack-1" className="w-[280px]"></img>
+
+                <button type="button" className="flex items-center text-white hover:text-[#0B0C2A] border-[#C4F7BC] hover:[#0B0C2A] hover:bg-[#C4F7BC] active:ring-4 active:ring-[#C4F7BC] active:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-10 mx-auto border-2">300 coins</button>
                 <button type="button" className="flex items-center text-white hover:text-[#0B0C2A] border-[#C4F7BC] hover:[#0B0C2A] hover:bg-[#C4F7BC] active:ring-4 active:ring-[#C4F7BC] active:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-8 mx-auto border-2">300 coins</button>
             </div>
             {/* Add buttons below */}
             {/* 2nd pack */}
             <div className="">
-                <div className="flex h-[400px] w-[260px] bg-[#0B0C2A] border-4 border-[#C4F7BC] rounded-lg p-4">
+                {/* <div className="flex h-[400px] w-[260px] bg-[#0B0C2A] border-4 border-[#C4F7BC] rounded-lg p-4">
                     <img src="/assets/temp-card-pack-1.svg" height={400} width={260} alt="Temp-Card-Pack-1" className="w-full h-full"></img>
-                </div>
-                    <button type="button" className="flex items-center text-white hover:text-[#0B0C2A] border-[#C4F7BC] hover:[#0B0C2A] hover:bg-[#C4F7BC] active:ring-4 active:ring-[#C4F7BC] active:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-8 mx-auto border-2">300 coins</button>
+                </div> */}
+                <img src="/assets/temp-card-pack-1.svg" height={400} width={260} alt="Temp-Card-Pack-1" className="w-[280px]"></img>
+
+                    <button type="button" className="flex items-center text-white hover:text-[#0B0C2A] border-[#C4F7BC] hover:[#0B0C2A] hover:bg-[#C4F7BC] active:ring-4 active:ring-[#C4F7BC] active:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-10 mx-auto border-2">300 coins</button>
                     <button type="button" className="flex items-center text-white hover:text-[#0B0C2A] border-[#C4F7BC] hover:[#0B0C2A] hover:bg-[#C4F7BC] active:ring-4 active:ring-[#C4F7BC] active:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-8 mx-auto border-2">300 coins</button>
                     </div>
 
             {/* 3rd pack */}
             <div className="">
-                <div className="flex h-[400px] w-[260px] bg-[#0B0C2A] border-4 border-[#C4F7BC] rounded-lg p-4">
+                {/* <div className="flex h-[400px] w-[260px] bg-[#0B0C2A] border-4 border-[#C4F7BC] rounded-lg p-4">
                     <img src="/assets/temp-card-pack-1.svg" height={400} width={260} alt="Temp-Card-Pack-1" className="w-full h-full"></img>
-                </div>
-                <button type="button" className="flex items-center text-white hover:text-[#0B0C2A] border-[#C4F7BC] hover:[#0B0C2A] hover:bg-[#C4F7BC] active:ring-4 active:ring-[#C4F7BC] active:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-8 mx-auto border-2">300 coins</button>
-                <button type="button" className="flex items-center text-white hover:text-[#0B0C2A] border-[#C4F7BC] hover:[#0B0C2A] hover:bg-[#C4F7BC] active:ring-4 active:ring-[#C4F7BC] active:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-8 mx-auto border-2">300 coins</button>
+                </div> */}
+                <img src="/assets/temp-card-pack-1.svg" height={400} width={260} alt="Temp-Card-Pack-1" className="w-[280px]"></img>
+
+                    <button type="button" className="flex items-center text-white hover:text-[#0B0C2A] border-[#C4F7BC] hover:[#0B0C2A] hover:bg-[#C4F7BC] active:ring-4 active:ring-[#C4F7BC] active:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-10 mx-auto border-2">300 coins</button>
+                    <button type="button" className="flex items-center text-white hover:text-[#0B0C2A] border-[#C4F7BC] hover:[#0B0C2A] hover:bg-[#C4F7BC] active:ring-4 active:ring-[#C4F7BC] active:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-8 mx-auto border-2">300 coins</button>
 
             </div>
         </div>
