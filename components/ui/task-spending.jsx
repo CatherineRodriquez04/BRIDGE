d91511c6057910c4cb48c3a3e1e358a2723843c1
Hayes Chiasson <hayeschiasson1@yahoo.com>
2025-04-23 15:08:34 -0500

    scrolling / right page

--- components/ui/task-spending.jsx
+++ components/ui/task-spending.jsx
@@ -45,7 +45,7 @@ function TaskSpending() {
                 </div>
                 {/* Right Page */}
                 {/* Right Page */}
-                <div className="absolute h-full w-[50%] right-0 bg-[#0B0C2A] rounded-r-lg border-l-4">
+                <div className="absolute h-full w-[50%] right-0 bg-[#0B0C2A] rounded-r-lg border-l-4 overflow-scroll">
                     <div className="relative flex flex-col items-center top-[9%] h-[1%] text-4xl">
                         By Currency
                     </div>
@@ -65,6 +65,8 @@ function TaskSpending() {
                                 ###
                             </div>
                         </div>
+
+
                     </div>
 
                     {/* Overall Score:
