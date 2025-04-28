171a0f14f7392ef3d4d479f91c110cfd825b6e5b
Hayes Chiasson <hayeschiasson1@yahoo.com>
2025-04-28 17:43:51 -0500

    tactics task update

--- components/ui/task-tactics.jsx
+++ components/ui/task-tactics.jsx
@@ -10,13 +10,13 @@ function TaskTactics() {
                     </div>
                     {/* Scores(progressbars) */}
                     <div className="absolute w-full text-[#a3ffb0] top-[30%]">
-                        {/* <h2 className="text-center text-2xl tracking-wide mb-6">SCORE</h2> */}
+                        <h2 className="text-center text-4xl tracking-wide mb-6">By Category</h2>
 
-                        <div className="relative w-full mx-auto pt-16">
+                        <div className="relative w-full mx-auto pt-8">
                             {/* Top row - 3 items */}
                             <div className="flex justify-between mb-8">
                             <div className="text-center w-1/3">
-                                <button onclick="" className="text-3xl mb-1">Gatcha</button>
+                                <button onclick="" className="text-3xl mb-1">Packs/Gatcha</button>
                                 <p className="text-2xl ">1/5</p>
                                 {/* Fillable value later */}
                             </div>
@@ -40,7 +40,7 @@ function TaskTactics() {
                             </div>
 
                             <div className="text-center w-1/3 mr-[8.33%]">
-                                <button className="text-3xl mb-1">Collection</button>
+                                <button className="text-3xl mb-1">Artificial Scarcity</button>
                                 <p className="text-2xl">5/5</p>
                             </div>
                             </div>
@@ -48,12 +48,61 @@ function TaskTactics() {
                     </div>
                 </div>
                 {/* Right Page */}
-                <div className="absolute h-full w-[50%] right-0 bg-[#0B0C2A] rounded-r-lg flex flex-col justify-center border-l-4">
+                <div className="absolute h-full w-[50%] right-0 bg-[#0B0C2A] rounded-r-lg border-l-4 overflow-y-auto">
                     <div className="relative flex flex-col items-center top-[9%] h-[1%] text-4xl">
-                        Right
-                        {/* Show custom component for selected^^ */}
+                        Tactics Breakdown
                     </div>
+                    {/* Scores(progressbars) */}
+                    <div className="absolute w-full text-[#a3ffb0] top-[25%]">
+                        <h2 className="text-center text-3xl tracking-wide mb-6">Score Calculations</h2>
 
+                        <div className="relative w-full mx-auto">
+                            {/* Top row - 3 items */}
+                            <div className="flex justify-between mb-8">
+                            <div className="text-center w-1/3">
+                                <p className="text-3xl mb-1">Spending</p>
+                                <p className="text-2xl ">/500</p>
+                                {/* Fillable value later */}
+                            </div>
+
+                            <div className="text-center w-1/3">
+                                <p className="text-3xl mb-1">Gatcha</p>
+                                <p className="text-2xl ">/500</p>
+                            </div>
+
+                            <div className="text-center w-1/3">
+                                <p className="text-3xl mb-1">Tactics</p>
+                                <p className="text-2xl">%?/500</p>
+                            </div>
+                        </div>
+                            <h2 className="text-center text-3xl tracking-wide mb-6">Tactics Breakdown</h2>
+
+                                <h3 className="pl-12 text-3xl tracking-wide mb-6">Packs/Gacha</h3>
+                                <p className="pl-16 text-xl tracking-wide mb-6 whitespace-normal overflow-hidden max-w-[90%]">
+                                    The 
+                                </p>
+
+                                <h3 className="pl-12 text-3xl tracking-wide mb-6">Gatcha</h3>
+                                <p className="pl-16 text-xl tracking-wide mb-6 whitespace-normal overflow-hidden max-w-[90%]">
+                                    Gatcha,
+                                </p>
+
+                                <h3 className="pl-12 text-3xl tracking-wide mb-6">Tactics</h3>
+                                <p className="pl-16 text-xl tracking-wide mb-6 whitespace-normal overflow-hidden max-w-[90%]">
+                                    Fill in later
+                                </p>
+
+                                <h3 className="pl-12 text-3xl tracking-wide mb-6">Tactics</h3>
+                                <p className="pl-16 text-xl tracking-wide mb-6 whitespace-normal overflow-hidden max-w-[90%]">
+                                    Fill in later
+                                </p>
+
+                                <h3 className="pl-12 text-3xl tracking-wide mb-6">Tactics</h3>
+                                <p className="pl-16 text-xl tracking-wide mb-6 whitespace-normal overflow-hidden max-w-[90%]">
+                                    Fill in later
+                                </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
